2e65b74ae4de09b31db9324facc912e861800cdd
lufengkai <lufengkai.lfk@jeeseen.com>
2021-12-01 17:28:00 +0800

    refactor: 退出登录组件化

--- src/layout/components/Navbar/index.vue
+++ src/layout/components/Navbar/index.vue
@@ -4,24 +4,18 @@
       <!-- <i class="el-icon-s-fold collapse" @click="handleCollapse" /> -->
       {{ activeMenuName }}
     </div>
-    <el-dropdown class="navbar__dropdown" @command="handleCommand">
-      <el-image
-        class="image"
-        src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"
-      />
-      <i class="el-icon-caret-bottom" />
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item command="logOut">退出登录</el-dropdown-item>
-      </el-dropdown-menu>
-    </el-dropdown>
+
+    <Logout />
   </div>
 </template>
 
 <script>
 import store from "@/store/index";
+import Logout from "./Logout";
 
 export default {
   name: "Navbar",
+  components: { Logout },
   data() {
     return {};
   },
@@ -34,12 +28,6 @@ export default {
   methods: {
     handleCollapse() {
       store.state.isCollapse = !store.state.isCollapse;
-    },
-    handleCommand(command) {
-      // 退出登录
-      if (command === "logOut") {
-        this.$router.push({ name: "Login" });
-      }
     },
   },
 };
@@ -60,20 +48,6 @@ export default {
       font-size: 22px;
       cursor: pointer;
       margin-right: 20px;
-    }
-  }
-
-  &__dropdown {
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    .image {
-      width: 40px;
-      height: 40px;
-      border-radius: 10px;
-      margin-right: 10px;
     }
   }
 }
