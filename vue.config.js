6f2c0a5d600a68e6e58e0538fef2d22f0fa51675
lufengkai <zhanjiangmrlu@163.com>
2021-12-04 17:50:45 +0800

    feat: 关闭代理

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,16 @@ module.exports = {
   devServer: {
     host: "0.0.0.0", // localhost
     port: 8080, // 端口号
-    proxy: {
-      // 代理: 配置跨域请求
-      "/api": {
-        // target: 'https://192.168.137.57:55943', // 开发环境
-        // target: "https://192.168.137.57:55944", // 测试环境
-        ws: true,
-        changeOrigin: true,
-        pathRewrite: { "^/api": "" },
-      },
-    },
+    // proxy: {
+    //   // 代理: 配置跨域请求
+    //   "/api": {
+    //     // target: 'https://192.168.137.57:55943', // 开发环境
+    //     // target: "https://192.168.137.57:55944", // 测试环境
+    //     ws: true,
+    //     changeOrigin: true,
+    //     pathRewrite: { "^/api": "" },
+    //   },
+    // },
     open: true, //配置自动启动浏览器
   },
   chainWebpack: (config) => {
