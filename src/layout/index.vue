733f18b241e0827f7316419a12157bcd697186c2
xurui <xurui@pcitech.com>
2021-12-07 22:56:21 +0800

    fix:修复eslint报警

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,38 +1,38 @@
 <template>
-	<el-container class="layout">
-		<Sidebar />
-		<el-container class="container">
-			<el-header class="el-header">
-				<Navbar />
-			</el-header>
-			<el-main>
-				<router-view />
-			</el-main>
-		</el-container>
-	</el-container>
+  <el-container class="layout">
+    <Sidebar />
+    <el-container class="container">
+      <el-header class="el-header">
+        <Navbar />
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-import Sidebar from './components/Sidebar/index'
-import Navbar from './components/Navbar/index'
+import Sidebar from "./components/Sidebar/index";
+import Navbar from "./components/Navbar/index";
 
 export default {
-	name: 'Layout',
-	components: { Sidebar, Navbar },
-	data() {
-		return {}
-	},
-	methods: {
-		second(path) {
-			this.$router.push(path)
-		},
-	},
-}
+  name: "Layout",
+  components: { Sidebar, Navbar },
+  data() {
+    return {};
+  },
+  methods: {
+    second(path) {
+      this.$router.push(path);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .layout {
-	height: 100%;
+  height: 100%;
 }
 
 .el-header {
@@ -44,6 +44,6 @@ export default {
 }
 
 .container {
-	padding-left: 210px;
+  padding-left: 210px;
 }
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,121 +1,120 @@
 <template>
-	<div class="login">
-		<div class="form">		
-			<h1 class="title">大舜</h1>
-			<el-form
-				:model="ruleForm"
-				:rules="rules"
-				ref="ruleForm"
-				label-width="100px"
-				class="rule-form"
-				hide-required-asterisk
-			>
-				<el-form-item label="账号" prop="userName">
-					<el-input v-model="ruleForm.userName"></el-input>
-				</el-form-item>
-				<el-form-item label="密码" prop="password">
-					<el-input type="password" v-model="ruleForm.password"></el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button class="btn" type="primary" @click="submitForm('ruleForm')"
-						>登录</el-button
-					>
-				</el-form-item>
-			</el-form>
-		</div>
-	</div>
+  <div class="login">
+    <div class="form">
+      <h1 class="title">大舜</h1>
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="rule-form"
+        hide-required-asterisk
+      >
+        <el-form-item label="账号" prop="userName">
+          <el-input v-model="ruleForm.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="ruleForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn" type="primary" @click="submitForm('ruleForm')"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
-import { login } from '../../api/login'
+import { login } from "../../api/login";
 // import store from '../../store/index'
 export default {
-	name: 'Login',
-	data() {
-		return {
-			ruleForm: {
-				userName: 'admin',
-				password: '123456',
-			},
-			rules: {
-				name: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-				password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
-			},
-		}
-	},
+  name: "Login",
+  data() {
+    return {
+      ruleForm: {
+        userName: "admin",
+        password: "123456",
+      },
+      rules: {
+        name: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+      },
+    };
+  },
 
-	created() {},
+  created() {},
 
-	methods: {
-	submitForm(formName) {
-			this.$refs[formName].validate(valid => {
-				// if (!valid) return
-				if (valid) {
-					const params = {
-						userName: this.ruleForm.userName,
-						password: this.ruleForm.password,
-					}
-				 login(params)
-						.then(({ data = {} }) => {
-							const { result_code } = data
-									localStorage.setItem('code',result_code) || ''
-							const res2 = localStorage.getItem('code')
-							console.log(res2,'getStatus');
-							if(res2){
-							this.$router.push('/workbench/index')
-							}
-						})
-						.catch(err => {
-							console.log(err)
-						})
-				} else {
-					return
-				}
-				// this.$router.push("/workbench/index");
-			})
-		},
-		resetForm(formName) {
-			this.$refs[formName].resetFields()
-		},
-	},
-}
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        // if (!valid) return
+        if (valid) {
+          const params = {
+            userName: this.ruleForm.userName,
+            password: this.ruleForm.password,
+          };
+          login(params)
+            .then(({ data = {} }) => {
+              const { result_code } = data;
+              localStorage.setItem("code", result_code) || "";
+              const res2 = localStorage.getItem("code");
+              console.log(res2, "getStatus");
+              if (res2) {
+                this.$router.push("/workbench/index");
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          return;
+        }
+        // this.$router.push("/workbench/index");
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .login {
-	width: 100%;
-	height: 100%;
-	color: #fff;
-	background: #2d3a4b;
+  width: 100%;
+  height: 100%;
+  color: #fff;
+  background: #2d3a4b;
 
-	.title {
-margin-left: 240px;
-	}
+  .title {
+    margin-left: 240px;
+  }
 
-.form{
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	margin-left: -240px;
-	margin-top: -120px;
-}
+  .form {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -240px;
+    margin-top: -120px;
+  }
 
+  .rule-form {
+    width: 480px;
+    margin: 20px auto;
 
-	.rule-form {
-		width: 480px;
-		margin: 20px auto;
-
-		.btn {
-			width: 100%;
-		}
-	}
+    .btn {
+      width: 100%;
+    }
+  }
 }
 
 /deep/ {
-	.el-input__inner {
-		color: #fff;
-		background-color: #283443;
-		border: 1px solid #3e4957;
-	}
+  .el-input__inner {
+    color: #fff;
+    background-color: #283443;
+    border: 1px solid #3e4957;
+  }
 }
 </style>
